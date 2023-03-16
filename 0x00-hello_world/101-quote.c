0effb7c9ae0ef487c2f2797c16986118524c686f
tommy457 <boukhalfaml1011@gmail.com>
2023-03-16 03:48:53 -0700

    added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
