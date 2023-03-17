1e10762037ac8d3ec12826047f43eb0273ab3220
tommy457 <boukhalfaml1011@gmail.com>
2023-03-17 03:23:28 -0700

    added 4-print_alphabt file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
