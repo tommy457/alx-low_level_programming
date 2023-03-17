6b882cac7e39db35b1c6ec86060b69091fb45c6e
tommy457 <boukhalfaml1011@gmail.com>
2023-03-17 06:40:43 -0700

    added 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num = 48;
+	char ch = 'a'
+
+	while (num <= 57)
+	{
+		putchar (num);
+		num++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
