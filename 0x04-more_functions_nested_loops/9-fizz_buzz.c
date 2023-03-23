17d07b2ebf5181779d87934aa7ed9b5694dc9186
tommy457 <boukhalfaml1011@gmail.com>
2023-03-23 05:34:59 -0700

    added 9-fizz_buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,22 +5,21 @@
  *
  * Return: Nothing
  */
-
 inr main(void)
 {
 	int i = 1;
 
 	while (i <= 100)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3  == 0)
 		{
 			printf("Fizz");
 		}
