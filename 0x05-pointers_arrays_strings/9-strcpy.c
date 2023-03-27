63323a9f85bd6bbb21db05ab7badc508e3bfe8c6
tommy457 <boukhalfaml1011@gmail.com>
2023-03-27 07:13:54 -0700

    fixed bug in 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,19 +3,19 @@
 /**
  * _strcpy - copies the string pointed to by src to dest
  * @src: the string to be copied
- *
+ * @dest: copy of the string
  * Return: *dest
 **/
-
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	while (src[i] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
