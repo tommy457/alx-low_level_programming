b72be3c400f5f07fff5c55b4f709033878acff20
tommy457 <boukhalfaml1011@gmail.com>
2023-03-29 06:31:48 -0700

    added 6-cap_string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of arr string
+ * @s: The string that will be capitalized
+ *
+ * Return: The resulting string s.
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char arr[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (s[i] >= 'arr' && s[i] <= 'z')
+		{
+			if (i == 0)
+				s[i] -= 32;
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (arr[j] == s[i - 1])
+						s[i] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
