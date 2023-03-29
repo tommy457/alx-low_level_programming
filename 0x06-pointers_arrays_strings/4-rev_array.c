e0fe7fbafc0c4db81e64c696a86000fc43538075
tommy457 <boukhalfaml1011@gmail.com>
2023-03-29 03:55:36 -0700

    added 4-rev_array.c file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - concatenates 2 strings.
+ * @a: array to be reverced 
+ * @n: number of elements of the array
+ *
+ * Return: Nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int len, tmp, i, j;
+
+	i = n / 2;
+	len = n -1;
+	j = 0;
+	while(j < i)
+	{
+		tmp = a[j];
+		a[j] = a[len - j];
+		a[len - j] = tmp;
+		j++;
+	}
+}
