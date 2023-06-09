0bf97deb35756664ca4f450d0384d2d5260173ed
tommy457 <boukhalfaml1011@gmail.com>
2023-06-09 06:42:19 -0700

    i gotta watch my spelling

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: number of arguments.
+ * @argv: string pointer array to the arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i];
+	}
+	return (0);
+}
