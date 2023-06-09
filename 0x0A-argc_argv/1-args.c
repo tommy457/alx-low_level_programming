619341530e44b7a28c4ce3f4a7abcfb170125ecc
tommy457 <boukhalfaml1011@gmail.com>
2023-06-09 06:57:25 -0700

    fix bug in argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,8 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	if (argc)
-		printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
