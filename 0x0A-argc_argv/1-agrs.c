b0ff16dce73f727fce3e71547ed49f7a049c483d
tommy457 <boukhalfaml1011@gmail.com>
2023-06-09 06:36:29 -0700

    task 2 and fixed bug in task 1

---
+++ 0x0A-argc_argv/1-agrs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints the number of arguments passed into it. 
+ * @argc: number of arguments.
+ * @argv: string pointer array to the arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%d\n", argc);
+	return (0);
+}
