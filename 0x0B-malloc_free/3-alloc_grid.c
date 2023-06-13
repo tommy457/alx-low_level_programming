dadcb69d5efbf4696457b96e7cbf9f36ac6cca16
tommy457 <boukhalfaml1011@gmail.com>
2023-06-13 01:17:28 -0700

    fixed bug

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,9 +18,10 @@ int **alloc_grid(int width, int height)
 
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
 		free(arr);
 		return (NULL);
-
+	}
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
