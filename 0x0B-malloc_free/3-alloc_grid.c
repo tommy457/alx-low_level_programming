427329cb7d5f2bf5afe3589862c1846b775a075c
tommy457 <boukhalfaml1011@gmail.com>
2023-06-13 01:23:26 -0700

    hander malloc return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,12 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
