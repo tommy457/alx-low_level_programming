f75dcc92e44d2fbbeebbe97dc95b1e99916980b0
tommy457 <boukhalfaml1011@gmail.com>
2023-06-13 01:13:28 -0700

    handel free malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,6 +17,9 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		free(arr);
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
