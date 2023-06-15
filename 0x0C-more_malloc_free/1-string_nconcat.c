0db627c164f3e1ad7b42e4a8429d80bd14b7a2b3
tommy457 <boukhalfaml1011@gmail.com>
2023-06-15 03:52:29 -0700

    curlu brace

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,9 +26,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc((n + len + 1) * sizeof(char));
 
 	if (str == NULL)
+	{
 		free(str);
 		return (NULL);
-
+	}
 	if (s1 == NULL)
 		s1 = "";
 
