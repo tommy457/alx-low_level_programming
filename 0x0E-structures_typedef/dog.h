06b8c2c3a63f02ae0137cddacf693d0941dccefd
tommy457 <boukhalfaml1011@gmail.com>
2023-06-19 03:14:10 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - ....
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ *
+ * Description: ...
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
