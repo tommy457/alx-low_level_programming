cfc96b3c05f51207be0117489ff043004ab1fad8
tommy457 <boukhalfaml1011@gmail.com>
2023-06-21 03:12:37 -0700

    was not suppose to wright f logic

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,37 +1,4 @@
-#include "function_pointers.h"
-
-/**
- * _strlen - function calculate the length of string
- * @s: string
- *
- * Return: len
- */
-int _strlen(char *s)
-{
-	unsigned int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-
-/**
- * f - function that prints a name
- * @s: string
- *
- * Return: Nothing
- */
-void f(char *s)
-{
-	unsigned int i, len = _strlen(s);
-
-	for (i = 0; i <= len; i++)
-		_putchar(s[i]);
-}
-
+include "function_pointers.h"
 
 /**
  * print_name - function that prints a name
