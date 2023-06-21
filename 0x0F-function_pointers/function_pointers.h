59f4f5cdaf0c13d3944f187377023b9df7730e63
tommy457 <boukhalfaml1011@gmail.com>
2023-06-21 03:06:21 -0700

    fixed big in function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
