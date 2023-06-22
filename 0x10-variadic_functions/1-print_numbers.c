e72bd26d58852f28b42dd530a1d00e1904d2244c
tommy457 <boukhalfaml1011@gmail.com>
2023-06-22 03:12:58 -0700

    fix logig in 1-print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,18 +17,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		separator = "";
-
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
-		if (i == n - 1)
+		printf("%d", x);
+		if (i != n - 1 && separator != NULL)
 		{
-			printf("%d\n", x);
-			break;
+			printf("%s", separator);
 		}
-		printf("%d%s", x, separator);
 	}
+	printf("\n");
 	va_end(args);
 }
