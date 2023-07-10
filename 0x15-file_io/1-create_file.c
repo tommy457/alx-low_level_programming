bc1c9837b2a2400e9193051123bcb1c51483844f
tommy457 <boukhalfaml1011@gmail.com>
2023-07-10 03:46:30 -0700

    init len and return -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,16 +6,16 @@
  * @filename: filename to be created.
  * @text_content: is a NULL terminated string to write to the file.
  *
- * Return: 0 , 1 or -1.
+ * Return: 1 or -1.
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t file_open;
-	int len;
+	int len = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	file_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
