4e5aab54e4e853cb7a6ebd2fc9bcef27ff1665b3
tommy457 <boukhalfaml1011@gmail.com>
2023-09-07 09:56:37 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *HashTable;
 
 	HashTable = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (HashTable == NULL)
 		return (NULL);
 
 	HashTable->size = size;
