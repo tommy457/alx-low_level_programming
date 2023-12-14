c1dae13b378adbf707d2ab64676af552ef28b8dc
tommy457 <boukhalfaml1011@gmail.com>
2023-12-14 01:50:16 -0800

    re-factor and optimaze

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,13 +1,12 @@
 #include "search_algos.h"
 
-listint_t *linear_search_(
-		listint_t *list, size_t start, size_t end, int value);
+listint_t *linear_search_(listint_t *list, listint_t *jump_, int value);
 /**
  * jump_list - Searches for a value in an list of integers
  * using the Jump search algorithm.
  *
- * @list: pointer to the first element of the array.
- * @size: number of elements in array.
+ * @list: pointer to the first element of the list.
+ * @size: number of elements in list.
  * @value: value to search for.
  *
  * Return: return a pointer to the first node where value is located or NULL.
@@ -15,58 +14,45 @@ listint_t *linear_search_(
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t tmp_idx, idx, jump = sqrt(size);
-	listint_t *jump_ = list, *curr = list;
+	size_t tmp_idx = 0, end = 0, jump = sqrt(size);
+	listint_t *curr = list, *jump_ = list;
 
 	if (!list)
 		return (NULL);
 
-	for (idx = curr->index; jump_; idx += jump)
+	for (; jump_->next ;)
 	{
-		tmp_idx = 0;
 		while (tmp_idx < jump && jump_->next)
 		{
 			jump_ = jump_->next;
 			tmp_idx++;
 		}
 		tmp_idx = 0;
-
-		if (idx + jump > size)
+		printf("Value checked array [%lu] = [%d]\n", jump_->index, jump_->n);
+		if (jump_->n < value && jump_->next)
 		{
-			printf("Value found between indexes [%lu] and [%lu]\n",
-					idx - jump, size - 1);
-			return (linear_search_(curr, idx - jump, size - 1, value));
+			curr = jump_;
 		}
-
-		printf("Value checked at index [%lu] = [%d]\n", jump_->index, jump_->n);
-		if (curr->n <= value && jump_->n >= value)
-		{
-			printf("Value found between indexes [%lu] and [%lu]\n",
-					idx, idx + jump);
-			return (linear_search_(curr, idx, idx + jump - 1, value));
-		}
-		while (tmp_idx < jump && jump_->next)
-		{
-			curr = curr->next;
-			tmp_idx++;
-		}
+		else
+			break;
 	}
-	return (NULL);
+	end = jump_->index < size ? jump_->index : size - 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", curr->index, end);
+	return (linear_search_(curr, jump_, value));
 }
 
 /**
  * linear_search_ - Searches for a value in an array of list
  * using the Linear search algorithm.
  *
- * @list: pointer to the first element of the array.
- * @start: index where to start searching.
- * @end: index where to stop searching
+ * @list: pointer to the node where to start searching.
+ * @jump_: pointer to the node where to stop searching.
  * @value: value to search for.
  *
  * Return: return a pointer to the first node where value is located or NULL.
  */
 
-listint_t *linear_search_(listint_t *list, size_t start, size_t end, int value)
+listint_t *linear_search_(listint_t *list, listint_t *jump_, int value)
 {
 	size_t idx;
 	listint_t *curr = list;
@@ -74,12 +60,11 @@ listint_t *linear_search_(listint_t *list, size_t start, size_t end, int value)
 	if (!list)
 		return (NULL);
 
-	for (idx = start; idx <= end; idx++)
+	for (idx = curr->index; idx <= jump_->index; idx++, curr = curr->next)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", idx, curr->n);
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
 		if (curr->n == value)
 			return (curr);
-		curr = curr->next;
 	}
 	return (NULL);
 }
